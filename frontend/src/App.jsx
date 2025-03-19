0c1149d121a62177a87dad8289600861f1ca3d7f
Abhinay Ahirwar <you@example.com>
2025-03-19 23:48:25 +0530

    first commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div className='text-white bg-black text-center'>
+      hello, bro continue the work @!
+    </div>
+  )
+}
+
+export default App
